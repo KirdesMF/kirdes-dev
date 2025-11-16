2e2fce5bc2d4e062edc3621bbbd964750fefd48a
KirdesMF <cedgourville@gmail.com>
2025-11-16 01:45:51 +0100

    feat: i18n structure

---
+++ src/i18n/ui.ts
@@ -0,0 +1,29 @@
+export const languages = {
+	en: "English",
+	fr: "French",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+	en: {
+		"nav.home": "Home",
+		"nav.snippets": "Snippets",
+		"nav.gallery": "Gallery",
+		"nav.blog": "Blog",
+		"hello.home": "Hello Home",
+		"hello.snippets": "Hello Snippets",
+		"hello.gallery": "Hello Gallery",
+		"hello.blog": "Hello Blog",
+	},
+	fr: {
+		"nav.home": "Home",
+		"nav.snippets": "Snippets",
+		"nav.gallery": "Galerie",
+		"nav.blog": "Blog",
+		"hello.home": "Bonjour",
+		"hello.snippets": "Bonjour Snippets",
+		"hello.gallery": "Bonjour Galerie",
+		"hello.blog": "Bonjour Blog",
+	},
+} as const;
